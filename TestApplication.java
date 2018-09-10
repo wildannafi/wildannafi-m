f1380b154d38bea6c2e32397cfb89d4f4e3006c6
wildannafi <43104270+wildannafi@users.noreply.github.com>
2018-09-10 00:09:12 -0700

    Add files via upload

---
+++ TestApplication.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testapplication;
+
+/**
+ *
+ * @author USER
+ */
+public class TestApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int a = 6;
+        for (int i=0;i<a;i++) {
+            for (int k=0;k<i;k++){
+           System.out.print("*");
+            }
+            System.out.println();
+    }
+        
+    }
+    
+}
